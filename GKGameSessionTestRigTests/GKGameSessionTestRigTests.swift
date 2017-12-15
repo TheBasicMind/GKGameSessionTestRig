2c21d2e05ab4139e57ab39b5f8e42019a835528a
git <paul@radicalfraction.com>
2017-12-15 16:16:03 +0000

    Initial Commit

---
+++ GKGameSessionTestRigTests/GKGameSessionTestRigTests.swift
@@ -0,0 +1,36 @@
+//
+//  GKGameSessionTestRigTests.swift
+//  GKGameSessionTestRigTests
+//
+//  Created by Paul Lancefield on 15/12/2017.
+//  Copyright © 2017 Paul Lancefield. All rights reserved.
+//
+
+import XCTest
+@testable import GKGameSessionTestRig
+
+class GKGameSessionTestRigTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
